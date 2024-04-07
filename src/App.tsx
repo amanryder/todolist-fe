1e967dac7dc9d3cd3a4fd90f890ae54cc4db9df6
amanryder <getahunamanuel@gmail.com>
2024-04-07 13:25:20 +0300

    initial commit :)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex justify-center">
+        <div className="pt-10 text-2xl font-semibold">Todo List</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
